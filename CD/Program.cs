d7120c11f7413eb510a7815f3a2d245da417add9
Tom Ilestrand <tomilestrand@gmail.com>
2019-05-09 11:21:37 +0200

    initial commit

---
+++ CD/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matches, N, M;
+            HashSet<string> collection;
+            string input;
+            string[] parameters;
+            while ((input = Console.ReadLine()) != "0 0")
+            {
+                parameters = input.Split(' ');
+                N = int.Parse(parameters[0]);
+                M = int.Parse(parameters[1]);
+
+                matches = 0;
+                collection = new HashSet<string>();
+
+                for (; N > 0; N--)
+                {
+                    collection.Add(Console.ReadLine());
+                }
+
+                for (; M > 0; M--)
+                {
+                    if (collection.Contains(Console.ReadLine()))
+                        matches++;
+                }
+                Console.WriteLine(matches);
+            }
+        }
+    }
+}
